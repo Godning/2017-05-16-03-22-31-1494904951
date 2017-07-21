32fd34db27532b71f0d533fc6b54ee3e8631048a
Godning <736397792@qq.com>
2017-07-21 08:13:27 +0000

    update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,27 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    //console.log("Debug Info");
+    var digital = [
+    ['._.', '...', '._.', '._.', '...', '._.', '._.', '._.', '._.', '._.'],
+    ['|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|'],
+    ['|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|']];
+    var num_list = [];
+    for(let i of num){
+        num_list.push(i-'0');
+    }
+    var total = "";
+    for(let y of [0,1,2]){
+        var i=1;
+        for(let x of num_list){
+            if(i != num_list.length){
+                i++;
+                total += digital[y][x]+' ';
+            }else{
+                total += digital[y][x];
+            }
+            
+        }
+        total += '\n';
+    }
+    //console.log(total);
+    return total; 
 };
